0a223b0d73ac8f9dcd013ef3f45849022cc3e194
Sylvia Ma <xiangwma@umich.edu>
2016-12-16 17:24:43 -0600

    init

---
+++ cat/Program.cs
@@ -0,0 +1,41 @@
+﻿using zoo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zoo
+{
+    interface IAnimal
+    { 
+        string Sound();
+        int Age();
+    }
+
+    class Animal: IAnimal
+    {
+        public string Sound()
+        {
+            return "Meow";
+        }
+
+        public int Age()
+        {
+            return 2;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Animal cat = new Animal();
+            string cat_sound=cat.Sound();
+            cat.Age();
+            Console.WriteLine(cat_sound);
+        }
+    }
+}
